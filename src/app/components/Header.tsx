dafc62ee47241a37c380c4f3210136ac7d159eb9
luctonio <luctognoli@gmail.com>
2024-03-02 22:10:28 -0300

    Agregando header y limpiando el proyecto

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+import "../assets/styles/page.header.css";
+const Header = () => {
+
+  return (
+    <header className="header">
+      <h1>Hola</h1>
+    </header>
+  )
+}
+export default Header;
